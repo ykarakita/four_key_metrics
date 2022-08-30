5b2a7c8f690d55e163fbd7efecc8abff8008ce38
ykarakita <taf.749@gmail.com>
2022-08-30 10:57:37 +0900

    csv に出力する際、リードタイムは日数に変換する

--- csv_generator.rb
+++ csv_generator.rb
@@ -2,9 +2,18 @@ require "csv"
 
 class CsvGenerator
   def self.generate!(file_name:, header:, rows:)
+    lead_time_column_index = header.find_index("lead_time")
+
     generated_csv = CSV.generate(headers: header, write_headers: true, encoding: Encoding::UTF_8) do |csv|
       rows.each do |row|
-        converted_row = row.map { _1.is_a?(Time) ? _1.localtime.strftime("%Y-%m-%d %H:%M:%S") : _1 }
+        converted_row = row.map.with_index do |value, idx|
+          # lead_time は日で出力します
+          next (value / 60 / 60 / 24).round(1) if idx == lead_time_column_index
+          # Time 型の値は CSV で扱いやすい形にします
+          next value.localtime.strftime("%Y-%m-%d %H:%M:%S") if value.is_a?(Time)
+
+          value
+        end
         csv << converted_row
       end
     end
